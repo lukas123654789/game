a4a3ab3b8e9eb3288c4a13e3998bed1773da338a
Your Name <you@example.com>
2026-06-26 11:55:53 +0200

    pridal som monstra

--- monster.py
+++ monster.py
@@ -1,19 +1,34 @@
 class Vlado:
     def __init__(self):
-        pass
+        self.level = 4
+        self.hp = 200
+        self.ability = "fatal rap game"
+        self.damage = 30
 
 class Danko:
     def __init__(self):
-        pass
+        self.level = 2
+        self.hp = 90
+        self.ability = "kradez"
+        self.damage = 20
 
 class Stefan:
     def __init__(self):
-        pass
+        self.level = 8
+        self.hp = 400
+        self.ability = "krik"
+        self.damage = 50
 
 class Klad:
     def __init__(self):
-        pass
+        self.level = 6
+        self.hp = 100
+        self.ability = "fatal penetration"
+        self.damage = 200
 
 class SorbyBarber:
     def __init__(self):
-        pass
+        self.level = "final boss"
+        self.hp = 650
+        self.ability = "strih"
+        self.damage = 85

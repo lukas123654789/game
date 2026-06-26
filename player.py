3e9b9c619c6986ec631d8628a2d5e7c19c8db4d3
Your Name <you@example.com>
2026-06-26 11:39:13 +0200

    added player

--- player.py
+++ player.py
@@ -1,3 +1,20 @@
 class Player:
-    def __init__(self):
+    def __init__(self, player_name):
+        self.name = player_name
+        self.level = 1
+        self.hp = 100
+        self.damage = 25
+
+    def level_up(self):
+        self.level += 1
+        self.hp += 75
+        self.damage += 10
+
+    def attack(self):
+        pass
+        
+    def ultimate_attack(self):
+        pass
+
+    def run_away(self):
         pass

6e524c728bdde7f73c89fd1fc85084a7c1c11b4c
Your Name <you@example.com>
2026-06-26 12:18:13 +0200

    vybavene

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from player import Player
 from monster import *
+import random
 
-monster1 = Vlado()
+
+
+player_name = input("Enter your name: ")
+
+monster1 = Danko()
+monster2 = Vlado()
+monster3 = Stefan()
+monster4 = Klad()
+monster5 = SorbyBarber()
+
+player1 = Player(player_name)
+monsters = [monster1, monster2, monster3, monster4]
+enemy = monsters[random.randint(0,3)]
+gane_on = True
+while gane_on:
+    break
+
+print(enemy.ability)

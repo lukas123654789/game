7fdbe014d503d90509786d158c8b39f15488aa3f
Your Name <you@example.com>
2026-06-26 10:58:18 +0200

    inital commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from player import Player
+from monster import *
+
+monster1 = Vlado()
---
+++ monster.py
@@ -0,0 +1,19 @@
+class Vlado:
+    def __init__(self):
+        pass
+
+class Danko:
+    def __init__(self):
+        pass
+
+class Stefan:
+    def __init__(self):
+        pass
+
+class Klad:
+    def __init__(self):
+        pass
+
+class SorbyBarber:
+    def __init__(self):
+        pass
